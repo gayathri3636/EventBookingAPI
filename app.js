13617e15c0e5718c836cd901cafd320eee4a76dc
Gayathri Nannapaneni <gayathri.pegasian@gmail.com>
2020-09-15 13:35:32 -0400

    added schema and resolvers

--- app.js
+++ app.js
@@ -1,14 +1,36 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const graphqlHttp = require('express-graphql').graphqlHTTP;
+const {buildSchema} = require('graphql');
 
 const app = express();
 
 app.use(bodyParser.json());
-const PORT = 5000;
+const PORT = process.env.PORT||5000;
 
-app.get('/', (req, res) => {
-    res.send({
-        message:"Hello"
-    })
-})
+app.use('/graphql', graphqlHttp({
+     schema:buildSchema(`
+        type RootQuery {
+            events:[String!]!
+        }
+        type RootMutation {
+            createEvent(name:String):String
+        }
+        schema {
+            query:RootQuery
+            mutation:RootMutation
+        }
+     `),
+     rootValue:{
+         events: () => {
+             return ['Cooking', 'sailing', 'coding']
+         },
+         createEvent: (args) => {
+            const eventName = args.name;
+            return eventName;
+         }
+     },
+     graphiql:true
+}));
+
 app.listen(PORT, () => console.log(`server started on running on ${PORT}`));
