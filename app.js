86630bcfd2b407daf2779a7e706ef136279c2f34
Gayathri Nannapaneni <gayathri.pegasian@gmail.com>
2020-09-16 11:45:46 -0400

    added users

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const bcrypt = require('bcryptjs');
 const graphqlHttp = require('express-graphql').graphqlHTTP;
 const { buildSchema } = require('graphql');
 const mongoose = require('mongoose');
 const Event = require('./models/event');
+const User = require('./models/user');
 const app = express();
 
 app.use(bodyParser.json());
@@ -20,17 +22,27 @@ app.use(
             price:Float!
             date:String!
         }
+        type User {
+          _id:ID
+          email:String!
+          password:String
+        }
         input EventInput {
             title:String!
             description:String!
             price:Float!
             date:String!
         }
+        input UserInput {
+          email:String!
+          password:String!
+        }
         type RootQuery {
             events:[Event!]!
         }
         type RootMutation {
-            createEvent(eventInput:EventInput):String
+            createEvent(eventInput:EventInput):Event
+            createUser(userInput:UserInput):User
         }
         schema {
             query:RootQuery
@@ -50,31 +62,55 @@ app.use(
           });
       },
       createEvent: (args) => {
-        //we created this object to save event
-        // const event = {
-        //   _id: Math.random().toString(),
-        //   title: args.eventInput.title,
-        //   description: args.eventInput.description,
-        //   price: +args.eventInput.price,
-        //   date: args.eventInput.date,
-        // };
-        // console.log(args);
-        //Now saving in database
         const event = new Event({
           title: args.eventInput.title,
           description: args.eventInput.description,
           price: +args.eventInput.price,
           date: new Date(args.eventInput.date),
+          creator: '5f62326281ee1b12906ffa6f',
         });
+        let createdEvent;
         return event
           .save()
           .then((result) => {
-            console.log(result);
-            return { ...result._doc, _id: result._doc._id.toString() };
+            createdEvent = { ...result._doc, _id: result._doc._id.toString() };
+            return User.findById('5f62326281ee1b12906ffa6f');
+          })
+          .then((user) => {
+            if (!user) {
+              throw new Error('user not found');
+            }
+            user.createdEvents.push(event);
+            return user.save();
+          })
+          .then((result) => {
+            return createdEvent;
           })
           .catch((err) => console.log(err));
         throw err;
         return event;
+      },
+      createUser: (args) => {
+        return User.findOne({ email: args.userInput.email })
+          .then((user) => {
+            if (user) {
+              throw new Error('User exists already');
+            }
+            return bcrypt.hash(args.userInput.password, 12);
+          })
+          .then((hashedPassword) => {
+            const user = new User({
+              email: args.userInput.email,
+              password: hashedPassword,
+            });
+            return user.save();
+          })
+          .then((result) => {
+            return { ...result._doc, password: null, _id: result.id };
+          })
+          .catch((err) => {
+            throw err;
+          });
       },
     },
     graphiql: true,
